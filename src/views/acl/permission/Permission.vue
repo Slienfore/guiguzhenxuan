b5a23ae723556de2edf808d427e469a3943f4ca2
Slienfore <Slienfore@163.com>
2024-05-04 17:55:13 +0800

    feat: 更新表单静态搭建

--- src/views/acl/permission/Permission.vue
+++ src/views/acl/permission/Permission.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts" name="Menu">
 import { ref, onMounted } from 'vue'
-import type { PermissionList, PermissionResponseData } from '@/api/acl/menu/type'
+import type { Permission, PermissionList, PermissionResponseData } from '@/api/acl/menu/type'
 import { reqAllPermission } from '@/api/acl/menu';
 
 const permissionList = ref<PermissionList>([])
@@ -16,6 +16,18 @@ const getPermission = async () => {
         permissionList.value = res.data
     }
 }
+
+const dialogVisible = ref(true)
+// 添加权限
+const addPermission = () => {
+    dialogVisible.value = true
+}
+
+// 编辑菜单
+const updatePermission = (row: Permission) => {
+    dialogVisible.value = true
+
+}
 </script>
 
 <template>
@@ -28,14 +40,30 @@ const getPermission = async () => {
             <template #="{ row }">
                 <!-- 四级菜单为 功能, 一~三级菜单为 菜单 -->
                 <!-- 四级菜单不能够添加 -->
-                <el-button v-if="row.level !== 4" type="primary">{{ row.level === 3 ? '添加功能' : '添加菜单' }}</el-button>
+                <el-button @click="addPermission" v-if="row.level !== 4" type="primary">{{ row.level === 3 ? '添加功能' : '添加菜单' }}</el-button>
                 <!-- 一级菜单不能够编辑 | 删除 -->
-                <el-button v-if="row.level !== 1" type="warning" icon="Edit"></el-button>
+                <el-button @click="updatePermission(row)" v-if="row.level !== 1" type="warning" icon="Edit"></el-button>
                 <el-button v-if="row.level !== 1" type="danger" icon="Delete"></el-button>
             </template>
         </el-table-column>
     </el-table>
 
+    <el-dialog v-model="dialogVisible" title="添加菜单" width="30%">
+        <template #default>
+            <el-form label-position="top">
+                <el-form-item label="名称">
+                    <el-input placeholder="请输入..."></el-input>
+                </el-form-item>
+                <el-form-item label="权限">
+                    <el-input placeholder="请输入..."></el-input>
+                </el-form-item>
+            </el-form>
+        </template>
+        <template #footer>
+            <el-button>取消</el-button>
+            <el-button type="primary">确定</el-button>
+        </template>
+    </el-dialog>
 </template>
 
 <style scoped></style>
